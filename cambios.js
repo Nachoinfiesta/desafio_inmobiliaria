b4e1565eb526d91d228edaf84dbca704220b86a2
Nachoinfiesta <ignaciocabelloinfiesta@gmail.com>
2023-12-18 10:55:25 -0300

    primer commit

---
+++ cambios.js
@@ -0,0 +1,53 @@
+// propiedades.js
+
+// Arreglo de propiedades en venta
+const propiedades_venta = [
+    {
+      nombre: 'Casa Amarilla',
+      src: 'url_imagen_casa.jpg',
+      descripcion: 'Una casa encantadora rodeada de naturaleza',
+      ubicacion: 'Bosque Encantado, CA',
+      habitaciones: 3,
+      costo: 300000,
+      smoke: false,
+      pets: true
+    },
+    {
+        nombre: 'Casa lujosa',
+        src: 'url_imagen_casa.jpg',
+        descripcion: 'casa a toda prueba',
+        ubicacion: 'El General ',
+        habitaciones: 3,
+        costo: 300500,
+        smoke: false,
+        pets: true
+      },
+      {
+        nombre: 'Casa de verani',
+        src: 'url_imagen_casa.jpg',
+        descripcion: 'Una casa encantadora cerca de la playa',
+        ubicacion: 'Litoral Central',
+        habitaciones: 3,
+        costo: 333000,
+        smoke: false,
+        pets: true
+      },
+    
+  ];
+  
+  // Arreglo de propiedades en alquiler
+  const propiedades_alquiler = [
+    {
+      nombre: 'Apartamento Urbano',
+      src: 'url_imagen_apartamento.jpg',
+      descripcion: 'Moderno apartamento en el corazón de la ciudad',
+      ubicacion: 'Centro Urbano, CA',
+      habitaciones: 2,
+      costo: 1500,
+      smoke: true,
+      pets: false
+    },
+    // Agrega más propiedades según sea necesario
+  ];
+  
+  
